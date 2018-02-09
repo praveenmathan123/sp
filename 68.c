63eeaba70bd35a4d28d4747124787f6181d17469
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-09 14:32:37 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+
+    int a;
+    printf("enter the number:");
+    scanf("%d",&a);
+    if(a%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
+return 0;
+}
