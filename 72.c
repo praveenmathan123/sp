08d5dbdba6608ac6d08e33c7ab322c63e0dee222
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-19 16:02:52 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{int l=0;
+string a;
+gets(a);
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='o'a[i]=='u')
+{
+l++;
+}
+printf(%d",l);
+}
+}
