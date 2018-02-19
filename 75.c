0e4f7d6e0858acaf8db2b6de21cc14ce4a4bdf2a
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-19 16:23:45 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+void main() 
+{
+char s[15];
+int a,b;
+scanf("%d",s);
+a=strlen(s);
+if(a%2!=0)
+{
+b=a/2;
+s[b]='*';
+printf("%s",s);
+}
+else
+{
+b=a/2;
+s[b]='*';
+s[b-1]='*';
+printf("%s",s);
+}
+}
+return 0;
+}
