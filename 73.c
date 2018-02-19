448d74c89777b8105c2710c74e9792faf5acd000
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-19 16:09:36 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main() 
+{
+int a,b,c;
+printf("Enterb the number\n");
+scanf("%d",&a);
+printf("Enter the range");
+scanf("%d %d",&b,&c);
+if(a>b&&a<c)
+printf("Yes");
+else
+printf("No");
+}
+}
+return0;
+}
