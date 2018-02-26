e8cc93e4f2e292e529c394900d08edf671664f8c
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-26 16:06:17 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+float l,b,area=0;
+printf("\nEnter length & breadth");
+scanf("%f%f",&l,&b);
+area=l*b;
+printf("\nArea of the form: %0.5f",area);
+}
+return 0;
+}
+}
