5536d00879818c693ff9dac73d8e57fb87a12fea
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-03-12 15:47:19 +0530

    Create 86.c

---
+++ 86.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[100];
+    int l,i,j,count=0;
+    scanf("%s",s1);
+    l=strlen(s1);
+    for(i=0;i<l;i++)
+    {
+        for(j=i+1;j<l;j++)
+        {
+            if(s[i]==s[j])
+            {
+            count=1;
+            break;
+            }
+        
+            else
+            continue;
+        }
+    }
+    
+    if(count==0)
+    printf("yes");
+    else
+    printf("no");
+    return 0;
+}
+return 0;
+}
+}
+}
+}
+}
+}
