030793a6325e80bda2405da3fa009571d37454e7
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-03-14 15:40:36 +0530

    Create 88.c

---
+++ 88.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int num1,num2,max;
+scanf("%d %d",&num1,&num2);
+max=(num1>num2)?num1:num2;
+while(1)
+{
+	if(max%num1==0&&max%num2==0)
+	{
+		printf("%d",max);
+	}
+
+	++max;
+}
+
+
+	return 0;
+}
+return 0;
+}
+}
+}
+}
